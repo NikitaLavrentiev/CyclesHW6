a86a1446d8eeaf911bf0352f3f8dc5f5587b3390
NikitaLavrentiev <119487200+NikitaLavrentiev@users.noreply.github.com>
2022-12-07 11:36:31 +0300

    First done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,55 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
+        task6();
+        task7();
+    }
+
+    public static void task1() {
+// С помощью цикла for выведите в консоль все числа от 1 до 10.
+        System.out.println("\nЗадача_1");
+        for (int i = 0; i <= 10; i++)
+        System.out.println(i);
+    }
+
+    public static void task2() {
+// С помощью цикла for выведите в консоль все числа от 10 до 1.
+        System.out.println("\nЗадача_2");
+        for (int i = 10; i <= 0; i = (i-1)) //не работает
+            System.out.println(i);
+    }
+
+    public static void task3() {
+
+        System.out.println("\nЗадача_3");
+
+    }
+
+    public static void task4() {
+
+        System.out.println("\nЗадача_4");
+
+    }
+
+    public static void task5() {
+
+        System.out.println("\nЗадача_5");
+
+    }
+
+    public static void task6() {
+
+        System.out.println("\nЗадача_6");
+
+    }
+
+    public static void task7() {
+
+        System.out.println("\nЗадача_7");
+
     }
 }
