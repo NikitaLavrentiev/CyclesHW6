6a3e4ddfe33eb2d8b831ad3b3e9f7f7c15eab858
NikitaLavrentiev <119487200+NikitaLavrentiev@users.noreply.github.com>
2022-12-07 12:01:54 +0300

    Second task complete

--- src/Main.java
+++ src/Main.java
@@ -11,45 +11,45 @@ public class Main {
 
     public static void task1() {
 // С помощью цикла for выведите в консоль все числа от 1 до 10.
-        System.out.println("\nЗадача_1");
+        System.out.println("\nЗадача 1");
         for (int i = 0; i <= 10; i++)
         System.out.println(i);
     }
 
     public static void task2() {
 // С помощью цикла for выведите в консоль все числа от 10 до 1.
-        System.out.println("\nЗадача_2");
-        for (int i = 10; i <= 0; i = (i-1)) //не работает
+        System.out.println("\nЗадача 2");
+        for (int i = 10; i >= 1; i--)
             System.out.println(i);
     }
 
     public static void task3() {
 
-        System.out.println("\nЗадача_3");
+        System.out.println("\nЗадача 3");
 
     }
 
     public static void task4() {
 
-        System.out.println("\nЗадача_4");
+        System.out.println("\nЗадача 4");
 
     }
 
     public static void task5() {
 
-        System.out.println("\nЗадача_5");
+        System.out.println("\nЗадача 5");
 
     }
 
     public static void task6() {
 
-        System.out.println("\nЗадача_6");
+        System.out.println("\nЗадача 6");
 
     }
 
     public static void task7() {
 
-        System.out.println("\nЗадача_7");
+        System.out.println("\nЗадача 7");
 
     }
 }
